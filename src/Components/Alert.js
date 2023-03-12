e28bb99e60a48215919699643962fe9a0ce4448f
Subham Raj <glorious10info@gmail.com>
2023-03-12 14:09:57 +0530

    color of textArea and accordian changed in dark mode

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -8,9 +8,13 @@ function Alert(props) {
     }
 
   return (
-    props.alert && 
-        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+    <div style={{width: '100%', height: '40px'}}>
+        {
+            props.alert && 
+            <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+            <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+            </div>
+        }
     </div>
     )
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -82,7 +82,7 @@ export default function TextForm(props) {
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" id="textBox" 
-            onChange={handleOnChange} value={text} rows="8" style={{backgroundColor: props.mode==='dark'?'grey':'white', color:props.mode==='light'?'grey':'white'}}></textarea>
+            onChange={handleOnChange} value={text} rows="8" style={{backgroundColor: props.mode==='dark'?'#465579':'white', color:props.mode==='light'?'black':'white'}}></textarea>
             </div>
             <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
